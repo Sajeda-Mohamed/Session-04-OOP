c48e59b7778ccc5bbbde42a0e3150defebe2b3a0
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-13 17:56:10 +0200

    Add project files.

---
+++ Demo/Interfaces/IMyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interfaces
+{
+    internal interface IMyType
+    {
+        public int Salary { get; set; }
+        void MyFun();
+    }
+}

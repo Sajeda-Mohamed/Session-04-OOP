dc0085fcaf0050f0332fddcb2f7fc6fbf94882dc
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-15 18:38:30 +0200

    Shallow Copy & Deep Copy

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.Interfaces;
+using System.Text;
 
 namespace Demo
 {
@@ -48,19 +49,84 @@ namespace Demo
             //SeriesByTwo seriesByTwo = new SeriesByTwo();
             //Print10NumbersFromSeries(seriesByTwo);
 
-            SeriesByThree seriesByThree = new SeriesByThree();
-            Print10NumbersFromSeries(seriesByThree);
+            //ISeries seriesByThree = new SeriesByThree();
+            //Print10NumbersFromSeries(seriesByThree);
             #endregion
 
             #region Implicit & Explicit Implementation
             //AirPlane airPlane = new AirPlane();
             //airPlane.Forward
 
-            IFlyable AirPlane1 = new AirPlane();
-            AirPlane1.Forward();
+            //IFlyable AirPlane1 = new AirPlane();
+            //AirPlane1.Forward();
 
-            IMovable AirPlane2 = new AirPlane();
-            AirPlane2.Forward();
+            //IMovable AirPlane2 = new AirPlane();
+            //AirPlane2.Forward();
+            #endregion
+
+            #region Shallow Copy & Deep Copy
+            //int[] arr1 = { 1, 2, 3 };
+            //int[] arr2 = new int[3]; // 0 0 0
+            //Console.WriteLine($"Arr01 = {arr1.GetHashCode()}"); //54267293
+            //Console.WriteLine($"Arr02 = {arr2.GetHashCode()}"); //18643596
+
+            //Console.WriteLine("============================");
+            //Shallow Copy 
+            //arr2 = arr1;
+            //Console.WriteLine($"Arr01 = {arr1.GetHashCode()}"); //54267293
+            //Console.WriteLine($"Arr02 = {arr2.GetHashCode()}"); //54267293
+
+            //arr1[0] = 10;
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+
+            //Console.WriteLine($"Arr01 = {arr1.GetHashCode()}");
+            //Console.WriteLine($"Arr02 = {arr2.GetHashCode()}");
+
+            // Deep Copy
+            
+            //Console.WriteLine("==============================");
+            //Console.WriteLine($"Arr01 = {arr1.GetHashCode()}");
+            //Console.WriteLine($"Arr02 = {arr2.GetHashCode()}");
+
+            //arr1[0] = 10;
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+            //StringBuilder[] Name1 = new StringBuilder[1];
+            //Name1[0] = new StringBuilder("Omar");
+            //StringBuilder[] Name2 = new StringBuilder[1];
+
+            //Console.WriteLine(Name1.GetHashCode()); //54267293
+            //Console.WriteLine(Name2.GetHashCode()); //18643596
+            ////Shallow Copy
+            //Name2 = Name1;
+            //Console.WriteLine(Name1.GetHashCode()); //54267293
+            //Console.WriteLine(Name2.GetHashCode()); //54267293
+
+            //Name2[0].Append(" Ahmed");
+            //Console.WriteLine(Name1[0]);
+            //Console.WriteLine(Name2[0]);
+
+            //------------------------------------------------
+            //StringBuilder[] Name1 = new StringBuilder[1];
+            //Name1[0] = new StringBuilder("Omar");
+            //StringBuilder[] Name2 = new StringBuilder[1];
+
+            //Console.WriteLine(Name1.GetHashCode()); //54267293
+            //Console.WriteLine(Name2.GetHashCode()); //18643596
+            //Console.WriteLine("=================================");
+            //Name2 = (StringBuilder[])Name1.Clone();
+
+            //Console.WriteLine(Name1.GetHashCode()); //54267293
+            //Console.WriteLine(Name2.GetHashCode()); //18643596
+
+            //Console.WriteLine(Name1[0]);
+            //Console.WriteLine(Name2[0]);
+
+            //Name2[0].Append("Ahmed");
+            //Name1[0].Append("Ahmed");
             #endregion
         }
     }

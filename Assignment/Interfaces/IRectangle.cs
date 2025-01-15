a6e29a36928f6c2f29c85caa4f6fefbbede690b2
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-15 19:40:28 +0200

    Q1 IShape

---
+++ Assignment/Interfaces/IRectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Interfaces
+{
+    internal interface IRectangle : IShape
+    {
+        double Width { get; set; }
+        double Hight { get; set; }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,20 +1,36 @@
-﻿namespace Assignment
+﻿using Assignment.Interfaces;
+using System.Drawing;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //part1
+            #region Part1
             //q1 => b
             //q2 => a
             //q3 => b
-            //q4 => a
+            //q4 => b
             //q5 => d
             //q6 => a
-            //q7 => a
+            //q7 => b
             //q8 => a
             //q9 => b
-            //q10 => c
+            //q10 => c 
+            #endregion
+
+            #region Part2
+            #region Q1 IShape
+            //IShape Circle = new Circle(7);
+            //IShape Rectangle = new Rectangle(5, 4);
+
+            //Circle.DisplayShapeInfo();
+            //Rectangle.DisplayShapeInfo(); 
+            #endregion
+
+
+            #endregion
         }
     }
 }

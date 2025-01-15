980db78b508468ebcd6cecafb0eece07425d398c
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-15 12:36:04 +0200

    Interface

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Demo
+﻿using Demo.Interfaces;
+
+namespace Demo
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface
+            //IMyType myType = new IMyType();
+
+            MyType myType = new MyType();
+            //myType.MyFun();
+            //myType.Salary = 6000;
+            //myType.Print();
+
+            IMyType myType1 = new MyType();
+            //myType1.MyFun();
+            //myType1.Salary = 6000;
+            //myType1.Print();
+            #endregion
+
         }
     }
 }

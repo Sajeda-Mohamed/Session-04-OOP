3e1389c104dc8ecc606c19d6c9355eb129787043
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-15 18:59:27 +0200

    Part1

---
+++ Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //part1
+            //q1 => b
+            //q2 => a
+            //q3 => b
+            //q4 => a
+            //q5 => d
+            //q6 => a
+            //q7 => a
+            //q8 => a
+            //q9 => b
+            //q10 => c
+        }
+    }
+}
